83f8ee19326185f54019280ce861e629d883aadc
Adina Gabriela Iamandi <iamandi.adina95@gmail.com>
2023-01-26 15:32:44 +0100

    add responsive cards

--- index.js
+++ index.js
@@ -5,21 +5,18 @@ const booksshelf = async function () {
     });
     if (res.ok) {
       let booksList = await res.json();
-      let card = document.getElementById("card");
+      let card = document.getElementById("container-books");
       booksList.forEach((book) => {
         card.innerHTML += `
-        <div class="row">
-            <div class="col-12 col-md-2 col-lg-4">
-                <div class="card" style="width: 18rem;">
-                    <img src="${book.img}" class="card-img-top" alt="${book.title} pic">
-                    <div class="card-body">
+                <div class="card col m-2" style="width: 18rem;">
+                    <img src="${book.img}" class="card-img-top" alt="${book.title} pic" style="height: 65%">
+                    <div class="card-body d-flex flex-column justify-content-between">
                         <h5 class="card-title" id="title">${book.title}</h5>
                         <p class="card-text">${book.price}$</p>
                         <a href="#" class="btn btn-primary">Skip</a>
                     </div>
                 </div>
-            </div>
-        </div>`;
+            `;
       });
     } else {
       console.log("Something went wrong");
